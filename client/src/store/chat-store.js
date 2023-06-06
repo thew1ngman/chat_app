dc44332d3d8f6862778b098769d666b2ca4f6753
Lemuel <lemuel@flowerstore.ph>
2023-06-07 03:03:47 +0800

    init zustand

---
+++ client/src/store/chat-store.js
@@ -0,0 +1,8 @@
+import { create } from 'zustand';
+
+const useChatStore = create(set => ({
+    chats: [],
+    storeChat: (payload) => set(state => ({chats: [...state.chats, payload]}))
+}));
+
+export default useChatStore;
